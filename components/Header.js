13d226568caba6dfbbbec22b0d067630f409265a
Harsh Mathur <harsh@felicity.care>
2022-09-17 22:25:41 +0530

    temp logo added

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { HomeIcon, LightningBoltIcon, BadgeCheckIcon, CollectionIcon, SearchIcon
 
 function Header() {
   return (
-    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+    <header className="flex flex-col md:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
@@ -13,7 +13,9 @@ function Header() {
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
         <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
-      <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} alt="Hulu Logo" />
+      <h1 className="object-contain text-5xl font-semibold font-mono" src="https://links.papareact.com/ua6" width={200} height={100} alt="Hulu Logo" >
+        logo
+      </h1>
     </header>
   )
 }
